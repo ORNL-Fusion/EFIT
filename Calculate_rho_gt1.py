50ac10348475d7088e9be1ced11fd9afa1ff2bc6
ZekeUnterberg <unterbergea@ornl.gov>
2018-10-26 15:16:08 -0400

    script to get rho values outside of LCFS
    adapted from old GA IDL code by Porter et al.

--- Calculate_rho_gt1.py
+++ Calculate_rho_gt1.py
@@ -12,7 +12,8 @@ import scipy.interpolate as scinter
 import EFIT.load_gfile_d3d as loadg
 import MDSplus as mds
 
-def main(shot,time,Rin,Zin,psin=None,tree='EFIT01',server='atlas.gat.com'):
+
+def standalone(shot, time, Rin, Zin, psin=None, tree='EFIT01', server='atlas.gat.com'):
     pi = np.pi
 
     MDSplusCONN = mds.Connection(server)
@@ -26,7 +27,7 @@ def main(shot,time,Rin,Zin,psin=None,tree='EFIT01',server='atlas.gat.com'):
         f_psiN = scinter.Rbf(Rs[Rs_trunc], Zs[Rs_trunc], parmDICT['psiRZn'][Rs_trunc],
                              function='linear')
 
-        psin = f_psiN(Rin,Zin)
+        psin = f_psiN(Rin, Zin)
 
 #   Calculat toroidal flux and its integral
     tFlx = 0.0
@@ -45,8 +46,46 @@ def main(shot,time,Rin,Zin,psin=None,tree='EFIT01',server='atlas.gat.com'):
 #   now use a spline to determine eho at input positions
 
     xvect = np.arange(parmDICT['NR']) / (parmDICT['NR']-1.)
-    rhospl = scinter.interp1d(xvect, rhox,fill_value='extrapolate',kind='slinear')
+    rhospl = scinter.interp1d(xvect, rhox, fill_value='extrapolate', kind='slinear')
 
     rho = rhospl(psin)
 
-    return {'rhoN': rho, 'psiN': psin, 'rhox':rhox,'xvect':xvect}
+    return {'rhoN': rho, 'psiN': psin}
+
+
+def main(shot, time, Rin, Zin, psin=None, tree='EFIT01', server='atlas.gat.com'):
+    shot = int(shot)
+    import h5py
+
+    dict = standalone(shot, time, Rin, Zin, psin=None, tree='EFIT01', server=server)
+    tmpNAM = 'pyEFIT_rhoN_psiN_'+str(shot)+'.h5'
+    fstore = h5py.File(tmpNAM, 'w')
+    ds = fstore.create_dataset('rhoN', np.shape(dict['rhoN']), 'f')
+    ds[...] = dict['rhoN']
+
+    ds = fstore.create_dataset('psiN', np.shape(dict['psiN']), 'f')
+    ds[...] = dict['psiN']
+
+    fstore.close()
+    print("hdf5 saved")
+
+
+# --- Launch main() ----------------------------------------------------------------------
+if __name__ == '__main__':
+    import argparse
+    import textwrap
+    parser = argparse.ArgumentParser(description='Collects an EFIT gfile and calcs rhoN',
+                                     formatter_class=argparse.RawDescriptionHelpFormatter,
+                                     epilog=textwrap.dedent('''\
+                                     Examples: 166025 2500 --Rin 2.20 2.21 --Zin -.207 -.207 '''))
+
+    parser.add_argument('shot', help='DIII-D shot number', type=float)
+    parser.add_argument('time', help='EFIT time', type=float)
+    parser.add_argument('--Rin', help='R locations to calculate rhoN, psiN', required=True, nargs='+', default=[])
+    parser.add_argument('--Zin', help='Z locations to calculate rhoN, psiN', required=True, nargs='+', default=[])
+    parser.add_argument('-psin', help='list of psiN values to use instead of Rin & Zin', type=float, default=None)
+    parser.add_argument('-t', '--tree', help='DIII-D EFIT tree', type=str, default='EFIT01')
+    parser.add_argument('-s', '--server', help='how to access the altas database', type=str, default='atlas.gat.com')
+    args = parser.parse_args()
+
+    main(args.shot, args.time, args.Rin, args.Zin, psin=args.psin, tree=args.tree, server=args.server)
