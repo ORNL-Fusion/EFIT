99ea2a4841f009151bdb8a2044f017099aa730db
Andreas Wingen <wingen@fusion.gat.com>
2025-02-18 16:24:30 -0800

    Added ne correction and pressure extension from sum(n*T) as functionality. This can be useful for profiles from other sources too.

--- extend_profiles.py
+++ extend_profiles.py
@@ -166,13 +166,135 @@ def plotProfile(psi, pro, save  = False, tag = None, rawdata = None):
 	if save: plt.gcf().savefig(rawdata['key'] + 'Profile' + tag + '.eps', dpi = (300), bbox_inches = 'tight')
 
 
+def correct_ne(psi0, p, ne, psiEx, Te, Ti = None, asymptote = 0, correctionMargin = None):
+	"""		
+	Correct ne, so that p - sum(n*T) >= 0 for all points in p
+	This ignores any original ni profile and assumes ni = ne, as required in M3D-C1, also ignore any impurities
+	Then extrapolate ne using exponential decay and interpolate ne on an extended psi grid
+	psi0 = original psi grid for p and ne
+	p = pressure profile on psi0 grid
+	ne = electron density on psi0 grid
+	psiEx = inter- and extrapolated (aka extended) psi grid for temperature only
+	Te = electron temperature on extended psi grid
+	Ti = optional ion temperature on extended psi grid, if None: Ti = p/ne - Te, but then force Ti >= Te.min() and continue correcting ne with this assumed Ti
+	asymptote = ne in far SOL
+	correctionMargin = value < 1, but close to 1, default is 0.99, to multiply sum(n*T) so that sum(n*T) < p even for interpolated values.
+	"""
+	e = 1.60217663e-19
+	if correctionMargin is None: correctionMargin = 0.99
+	
+	# Spline te and ti for the original psi
+	fte = scinter.UnivariateSpline(psiEx, Te, s = 0)
+	tex = fte(psi0)*e
+	if Ti is None: 
+		Ti = (p/ne - tex)/e
+		idx = np.where(Ti < Te.min())[0]
+		if len(idx) > 0: Ti[idx] = Te.min()
+		tix = Ti*e
+	else: 
+		fti = scinter.UnivariateSpline(psiEx, Ti, s = 0)	
+		tix = fti(psi0)*e
+	
+	# with original ne get d = p - sum(n*T), with original p, and extended/splined T
+	d = p - ne * (tex + tix)
+	
+	# find points where d < 0
+	idx = np.where(d < 0)[0]
+	if len(idx) == 0: 
+		print('ne okay, no correction needed')
+		return	# done, d >=0 everywhere already
+	
+	# where d < 0 replace ne with new value so that  p - sum(n*T) >= 0 -> ne patched
+	ne0 = p / (tex+tix)		# this ne would make d = 0 everywhere
+	nePatched = ne.copy()
+	nePatched[idx] = ne0[idx] * correctionMargin	# give it a tiny margin
+	
+	# Use a monotonic interpolation for ni_patched -> upscale ni to extended psi grid
+	# !!!!!!! This interpolator does not overshoot like UnivariateSpline, but instead maintains a monotonic curve !!!!!!!!!!!!
+	# However, this is not as smooth as a regular spline. The first derivatives are guaranteed to be continuous, but the second derivatives may jump
+	f = scinter.PchipInterpolator(psi0, nePatched)
+	sol = psiEx > 1
+	psiCore = psiEx[~sol]
+	y = f(psiCore)
+	
+	# Point and derivative at separatrix using the extended psi grid
+	x1 = psiCore[-1]	# this should be  = 1
+	y1 = y[-1]
+	dx = psiCore[-1] - psiCore[-2]	# NOT equidistant
+	dy1 = (-y[-2] + y[-1])/dx		# 1st order only due to non-equidistant grid
+	
+	# Fit exponential decay f(x) = a*exp(b*x) + c; c = asymptote is given as input
+	c = asymptote
+	b = dy1/(y1 - c)
+	a = dy1/b * np.exp(-b*x1)
+	
+	# extend ni
+	f = lambda x: a*np.exp(b*x) + c
+	neNew = np.append(y, f(psiEx[sol]))
+	
+	# verify & update
+	fne = scinter.UnivariateSpline(psiEx, neNew, s = 0)
+	p2 = fne(psi0) * (tex + tix)  	# Use original psi grid
+	d2 = p - p2
+	if any(d2 < 0): print('Sum of n*T exceeds thermal pressure inside the separatrix. Check extended profiles.')
+	else: print('ne correction okay')
+	return neNew
 
 
+def checkExtension(psiEx, ne, Te, ni, Ti, pEx):
+	"""
+	Verify that p - sum(n*T) >= 0
+	Using the extended psi grid and original grid
+	"""
+	e = 1.60217663e-19
+	p = ne * Te*e + ni * Ti*e
+	d = pEx - p
+	if any(d < -1e-14):	# exclude machine rounding errors 
+		idx = np.where(d < 0)[0]
+		print('Sum of n*T exceeds thermal pressure inside the separatrix. Check extended profiles.')
+	else: 
+		print('Extension okay')
+	
+	import matplotlib.pyplot as plt
+	plt.figure()
+	plt.plot(psiEx, pEx*1e-3, 'k-', lw = 2, label = 'thermal pressure')
+	plt.plot(psiEx, p*1e-3, 'b-', lw = 2, label = 'sum(n*T)')
+	plt.plot(psiEx, d*1e-3, 'r-', lw = 2, label = 'difference')
+	if any(d < -1e-14): plt.plot(psiEx[idx], d[idx]*1e-3, 'rx')
+	plt.plot([0,psiEx.max()], [0,0], 'k--', lw = 1)
+	plt.xlim(0,psiEx.max())
+	plt.xlabel('$\\psi$')
+	plt.ylabel('p$_{th}$ [kPa]')
+	plt.legend()
+	return
 
 
+def extendPressure(psiEx, ne, Te, ni, Ti, psi0, p0):
+	"""
+	Extend the pressure using sum(n*T) for psi > 1
+	"""
+	e = 1.60217663e-19
+	p = ne * Te*e + ni * Ti*e
+	idx = np.where(psiEx > 1)[0]
+	psiNew = np.append(psi0, psiEx[idx])
+	pNew = np.append(p0, p[idx])
+	f = scinter.PchipInterpolator(psiNew,pNew)
+	pEx = f(psiEx)
+	checkExtension(psiEx, ne, Te, ni, Ti, pEx)
+	return pEx
 
 
-
+def writeProfile(x, y, key, tag = None, norm = None):
+	if tag is None: tag = ''
+	else: tag = '_' + tag
+	if norm is None:
+		if 'n' in key: norm = 1e20
+		else: norm = 1e3
+	with open('profile' + tag + '_' + str.lower(key),'w') as f:
+		#f.write('# ' + key + ' profile in ' + units + ' \n')
+		#f.write('# psi          ' + key + ' [' + units + '] \n')
+		for i in range(len(x)):
+			f.write(format(x[i],' 13.7e') + ' \t' + format(y[i]/norm,' 13.7e') + '\n')
 
 
 
